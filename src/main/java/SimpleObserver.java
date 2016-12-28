d22589bfcd22e6b07547c5edf774b8b5369d3d8c
Olivia Lennerö <olivialennero@gmail.com>
2016-12-28 12:41:15 +0100

    First commit

---
+++ src/main/java/SimpleObserver.java
@@ -0,0 +1,16 @@
+import rx.Observer;
+
+public class SimpleObserver implements Observer<String> {
+
+    public void onCompleted() {
+        System.out.println("completed...");
+    }
+
+    public void onError(Throwable throwable) {
+        System.out.println("error...");
+    }
+
+    public void onNext(String s) {
+        System.out.println(s);
+    }
+}
